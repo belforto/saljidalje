cdafc42f60f4b33c443d0d17d999348aa3a253e2
Marko <marko.ognjenovich@gmail.com>
2019-10-29 17:24:40 +0100

    fix3

---
+++ build/precache-manifest.352a9e78899d8864e7d22292a4bc1e44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5f74cfba15fe3606bffd5126c8bd57",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "1b5af1f3368f693ed6c9",
+    "url": "/react-redux/static/css/main.a14c3943.chunk.css"
+  },
+  {
+    "revision": "50d838058a063442f2b2",
+    "url": "/react-redux/static/js/2.9aee4f81.chunk.js"
+  },
+  {
+    "revision": "1b5af1f3368f693ed6c9",
+    "url": "/react-redux/static/js/main.06d6e5b5.chunk.js"
+  },
+  {
+    "revision": "4afab779f3f20e6d1231",
+    "url": "/react-redux/static/js/runtime-main.c62233bb.js"
+  }
+]);
