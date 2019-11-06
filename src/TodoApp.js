897aabcebb05207abdecfdaab3d064b9032a79d3
Marko <marko.ognjenovich@gmail.com>
2019-11-06 12:02:52 +0100

    path url fix try with harder solution 2

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -8,7 +8,7 @@ import "./styles.css";
 import TestComponent from "./components/TestComponent";
 import Navbar from "./components/dumb/Navbar";
 
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
 export default function TodoApp() {
