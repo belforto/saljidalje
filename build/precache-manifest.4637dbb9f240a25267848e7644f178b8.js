c89d2dc9540ca9d695a5f3e212df862e525f4292
Marko <marko.ognjenovich@gmail.com>
2019-11-06 11:39:29 +0100

    path url fix revert back

--- build/precache-manifest.4637dbb9f240a25267848e7644f178b8.js
+++ build/precache-manifest.4637dbb9f240a25267848e7644f178b8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f20ebac7fdc3781692f89b0f61f77f0",
+    "revision": "c3a59cc636a6df67f310503f987bc528",
     "url": "/react-redux/index.html"
   },
   {
-    "revision": "946842954d184fe0ef30",
+    "revision": "662f5dbbe074dc454ba6",
     "url": "/react-redux/static/css/main.a14c3943.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-redux/static/js/2.9aee4f81.chunk.js"
   },
   {
-    "revision": "946842954d184fe0ef30",
-    "url": "/react-redux/static/js/main.f5735275.chunk.js"
+    "revision": "662f5dbbe074dc454ba6",
+    "url": "/react-redux/static/js/main.41d66af3.chunk.js"
   },
   {
     "revision": "4afab779f3f20e6d1231",
