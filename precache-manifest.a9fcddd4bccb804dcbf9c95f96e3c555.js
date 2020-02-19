449bedced2e8c69582021e3b5a8c65ac2a01929c
ogi <marko.ognjenovich@gmail.com>
2020-02-19 13:12:52 +0100

    Updates

---
+++ precache-manifest.a9fcddd4bccb804dcbf9c95f96e3c555.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2dd03d7f86a6bada0d93bcd44bbe09",
+    "url": "/saljidalje/index.html"
+  },
+  {
+    "revision": "9c7c562080c72f5566b1",
+    "url": "/saljidalje/static/css/2.2508ce2e.chunk.css"
+  },
+  {
+    "revision": "4449d9a929ae909a5d75",
+    "url": "/saljidalje/static/css/main.5a1255fd.chunk.css"
+  },
+  {
+    "revision": "9c7c562080c72f5566b1",
+    "url": "/saljidalje/static/js/2.e3bacca9.chunk.js"
+  },
+  {
+    "revision": "4449d9a929ae909a5d75",
+    "url": "/saljidalje/static/js/main.ec28a436.chunk.js"
+  },
+  {
+    "revision": "98401b44d1042f0ffae7",
+    "url": "/saljidalje/static/js/runtime-main.31119b1c.js"
+  },
+  {
+    "revision": "66a63c417cbbaeb3adafc931d9124e98",
+    "url": "/saljidalje/static/media/10.66a63c41.png"
+  },
+  {
+    "revision": "b5ff36270d36afa4505feac3a64a6fc3",
+    "url": "/saljidalje/static/media/12.b5ff3627.png"
+  },
+  {
+    "revision": "6b37ab7c660dc5d66ee3ef36735fad0a",
+    "url": "/saljidalje/static/media/13.6b37ab7c.png"
+  },
+  {
+    "revision": "65faeb0887dab07d10e2d1db0a3017b3",
+    "url": "/saljidalje/static/media/16.65faeb08.png"
+  },
+  {
+    "revision": "f80a0650c9e2241283c0777b8b86c0e6",
+    "url": "/saljidalje/static/media/30.f80a0650.png"
+  },
+  {
+    "revision": "fec424d2b3b8e39892da59cc100fcf53",
+    "url": "/saljidalje/static/media/9.fec424d2.png"
+  },
+  {
+    "revision": "041ed64c4006e994244af9d50d9779cd",
+    "url": "/saljidalje/static/media/logo.041ed64c.png"
+  },
+  {
+    "revision": "8ad700281acd3ed5929d2687a543d101",
+    "url": "/saljidalje/static/media/wall.8ad70028.png"
+  }
+]);
