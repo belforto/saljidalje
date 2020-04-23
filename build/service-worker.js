1e43ea969d451bf6184c939ca9c637708a75d897
ogi <marko.ognjenovich@gmail.com>
2020-04-23 17:43:33 +0200

    u package json promjena https://belforto.github.io/saljidalje/ u https://belforto.github.io/ jer inače ne radi sa vanjskom domenom

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/saljidalje/precache-manifest.3405ca39c01ada103fc7112cfb747404.js"
+  "/precache-manifest.3512d17eaedb534d149ad56231974570.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saljidalje/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
