0e4a880a03a8e14067db920e208f2f550aa7e3df
ogi <marko.ognjenovich@gmail.com>
2020-05-04 10:44:50 +0200

    Updates

---
+++ precache-manifest.64dc4c2bde80ea666fc487c937438737.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411d2a84bf13c4a466e9ee7bbedc262a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc34c954c1f70cc56166",
+    "url": "/static/css/2.d65e727e.chunk.css"
+  },
+  {
+    "revision": "06a31d622174b5c8791a",
+    "url": "/static/css/main.626676e3.chunk.css"
+  },
+  {
+    "revision": "cc34c954c1f70cc56166",
+    "url": "/static/js/2.2c8ec6eb.chunk.js"
+  },
+  {
+    "revision": "06a31d622174b5c8791a",
+    "url": "/static/js/main.92470b2f.chunk.js"
+  },
+  {
+    "revision": "9606e7035817ffd824c2",
+    "url": "/static/js/runtime-main.7beda545.js"
+  },
+  {
+    "revision": "befa45399d56935c5eddc9b837693c6d",
+    "url": "/static/media/10.befa4539.jpg"
+  },
+  {
+    "revision": "fdc3a301f524ceac371fb53debe129fc",
+    "url": "/static/media/12.fdc3a301.jpg"
+  },
+  {
+    "revision": "c276ded5765a75d29fda3e3cd79f9a06",
+    "url": "/static/media/13.c276ded5.jpg"
+  },
+  {
+    "revision": "4f662a9f2964be1b33fd0eddbc933c68",
+    "url": "/static/media/16.4f662a9f.jpg"
+  },
+  {
+    "revision": "45ba8551b0946887a60ea7bc07334eda",
+    "url": "/static/media/30.45ba8551.jpg"
+  },
+  {
+    "revision": "f7d6c08be4820cc9d0593029dbb01268",
+    "url": "/static/media/9.f7d6c08b.jpg"
+  },
+  {
+    "revision": "00751c09b155b2ddd83a7657a753cbf8",
+    "url": "/static/media/eko.00751c09.jpg"
+  },
+  {
+    "revision": "fd47abc289c41ced9126b08d7e0e6d41",
+    "url": "/static/media/kids.fd47abc2.jpg"
+  },
+  {
+    "revision": "041ed64c4006e994244af9d50d9779cd",
+    "url": "/static/media/logo.041ed64c.png"
+  },
+  {
+    "revision": "28041b2d036bcbe543494048c5f29d1a",
+    "url": "/static/media/obuca.28041b2d.jpg"
+  },
+  {
+    "revision": "bb4fee17b3f45a8541094b3911437b99",
+    "url": "/static/media/odjeca.bb4fee17.jpg"
+  },
+  {
+    "revision": "8ad700281acd3ed5929d2687a543d101",
+    "url": "/static/media/wall.8ad70028.png"
+  }
+]);
