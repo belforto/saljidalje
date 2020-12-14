5e298d00be9a3c662035f4cd185f08081eb2bf04
belforto <marko.ognjenovich@gmail.com>
2020-12-14 23:18:26 +0100

    snow2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8b192e3553f1769233458aa4a31ca49.js"
+  "/precache-manifest.9770982f43c4fdb8e7831aa1d6573ebb.js"
 );
 
 self.addEventListener('message', (event) => {
